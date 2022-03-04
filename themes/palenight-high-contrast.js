c9fea7f08e7d4e34cafce42c46b1adcf36b55ded
Gabrz <37636455+Gabrz@users.noreply.github.com>
2022-03-04 10:20:50 +0100

    Added the high contrast version

---
+++ themes/palenight-high-contrast.js
@@ -0,0 +1,63 @@
+module.exports = {
+  name: 'material-palenight-high-contrast',
+  displayName: 'Material Palenight HC',
+  theme: {
+    background: {
+      default: '#292D3E',
+      success: '#C3E887',
+      notice: '#FFCB6B',
+      warning: '#F78C6C',
+      danger: '#FF5370',
+      surprise: '#C792EA',
+      info: '#82AAFF'
+    },
+    foreground: {
+      default: '#CCC',
+      success: '#FFF',
+      notice: '#FFF',
+      warning: '#FFF',
+      danger: '#FFF',
+      surprise: '#FFF',
+      info: '#FFF'
+    },
+    highlight: {
+      default: '#757CA1',
+      xxs: '#1B1E2B',
+      xs: '#11142199',
+      sm: '#3B404F',
+      md: '#757ca133',
+      lg: '#757CA199',
+      xl: '#757CA166'
+    },
+    rawCss: `
+      .tooltip, .dropdown__menu {
+        opacity: 0.95;
+      }
+    `,
+    styles: {
+      appHeader: {
+        background: {
+          default: '#1B1E2B'
+        },
+        foreground: {
+          default: '#757CA1'
+        }
+      },
+      sidebar: {
+        background: {
+          default: '#1B1E2B'
+        }
+      },
+      sidebarList: {
+        foreground: {
+          default: '#80CBC4'
+        }
+      },
+      transparentOverlay: {
+        background: {
+          default: '#292D3ECC'
+        }
+      }
+    }
+  }
+}
